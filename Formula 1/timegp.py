fd7dea7b4d6b9b0c0f95ef9c27c3903f58c5dc5f
suhroballamurodov <asunteam1824@mail.ru>
2023-11-02 23:37:27 +0500

    django_5__app

--- Formula 1/timegp.py
+++ Formula 1/timegp.py
@@ -9,4 +9,4 @@ class Time:
     def __init__(self, gp:GrandPrix, driver: Driver, tugash_vaqti:TimeForm) -> None:
             self._gp = gp
             self._driver = driver
-            self._tugash_vaqti = tugash_vaqtiga
+            self._tugash_vaqti = tugash_vaqti
---
+++ dictionary_app/uyga_vazifa_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UygaVazifa1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'uyga_vazifa_1'
